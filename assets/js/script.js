538f01f32ef89dad465f2707f66ff4e90baf227a
Kevin Reid <kevinnivek@me.com>
2022-06-11 15:13:06 -0400

    added  5 day forecase

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ var SearchCityButton = document.querySelector("#search-button");
 var SearchCityHeader = document.querySelector("#search-city");
 var currentTemperatureUL = document.querySelector("#current_temp_conditions");
 var nextDayDate = document.querySelector("#next_day_date");
+var secondDayDate = document.querySelector("#second_forecast_date");
+var thirdDayDate = document.querySelector("#third_forecast_date");
+var fourthDayDate = document.querySelector("#fourth_forecast_date");
+var fifthDayDate = document.querySelector("#fifth_forecast_date");
 var CurrentDate = document.querySelector("#current_date");
 
 var citySearchEntered = "";
@@ -30,6 +34,29 @@ var secondForecastTemp = "";
 var secondForecastWind = "";
 var secondForecastHumidity = "";
 var secondForecastConditions = "";
+
+// third forecast day variables
+var thirdForecastDate = "";
+var thirdForecastTemp = "";
+var thirdForecastWind = "";
+var thirdForecastHumidity = "";
+var thirdForecastConditions = "";
+
+// fourth forecast day variables
+var fourthForecastDate = "";
+var fourthForecastTemp = "";
+var fourthForecastWind = "";
+var fourthForecastHumidity = "";
+var fourthForecastConditions = "";
+
+// fifth forecast day variables
+var fifthForecastDate = "";
+var fifthForecastTemp = "";
+var fifthForecastWind = "";
+var fifthForecastHumidity = "";
+var fifthForecastConditions = "";
+
+
 
 // variables for API keys
 var apiKeyWeather = "feb921e24625822c8914d6709ecb623e";
@@ -62,17 +89,35 @@ var ConvertUnixDate = function()  {
     // Day
     var day = date.getDate();
     var dayTwo = date.getDate()+1;
+    var dayThree = date.getDate()+2;
+    var dayFour = date.getDate()+3;
+    var dayFive = date.getDate()+4;
 
     //Display date and time in DD-MM-yyyy format
 
     var convdataTime =  day +"/" + month + "/" + year ;
-    nextDateConverted = convdataTime;
-    console.log("the converted date is now" +convdataTime); 
+        nextDateConverted = convdataTime;
+        console.log("the converted date is now" +convdataTime); 
 
+    // convert date for second day forecast
     var convertdateSecond = dayTwo +"/" + month + "/" + year ;
-    secondForecastDate = convertdateSecond;
-    console.log("the converted second date is now" + convertdateSecond); 
+        secondForecastDate = convertdateSecond;
+        console.log("the converted second date is now" + convertdateSecond); 
 
+    // convert daye for third day forecast
+    var convertdateThird = dayThree +"/" + month + "/" + year ;
+        thirdForecastDate = convertdateThird;
+        console.log("the converted third date is now" + convertdateThird); 
+
+    // convert date for fourth day forecast
+    var convertdatefourth = dayFour +"/" + month + "/" + year ;
+        fourthForecastDate = convertdatefourth;
+        console.log("the converted fourth date is now" + convertdatefourth);
+
+    // convert date for fifth day forecast
+    var convertdatefifth = dayFive +"/" + month + "/" + year ;
+        fifthForecastDate = convertdatefifth;
+        console.log("the converted fifth date is now" + convertdatefifth);
 
 
 }
@@ -113,7 +158,7 @@ var displayCurrentWeather = function(data){
 
     // displaying day 2 of 5 day forecast
 
-    second_forecast_date.innerHTML = secondForecastDate;
+    secondDayDate.innerHTML = secondForecastDate;
     document.getElementById("second_forecast_temp").innerHTML = "Temp: " + secondForecastTemp + '&#8451';
     document.getElementById("second_forecast_wind").innerHTML = "Wind: " + secondForecastWind + 'MPH';
     document.getElementById("second_forecast_humidity").innerHTML = "Humidity: " + secondForecastHumidity;
@@ -125,8 +170,49 @@ var displayCurrentWeather = function(data){
         secondForecastIcon.dataset.icon = "fluent:weather-cloudy-48-filled";
     } else  secondForecastIcon.dataset.icon ="material-symbols:wb-sunny-outline-rounded";
 
-}
 
+    // displaying day 3 of 5 day forecast
+
+    thirdDayDate.innerHTML = thirdForecastDate;
+    document.getElementById("third_forecast_temp").innerHTML = "Temp: " + thirdForecastTemp + '&#8451';
+    document.getElementById("third_forecast_wind").innerHTML = "Wind: " + thirdForecastWind + 'MPH';
+    document.getElementById("third_forecast_humidity").innerHTML = "Humidity: " + thirdForecastHumidity;
+
+    var thirdForecastIcon = document.getElementById("third_forecast_icon")
+    if (thirdForecastConditions === "rain"){
+         thirdForecastIcon.dataset.icon = "fa-solid:cloud-rain";
+    } else if (thirdForecastConditions === "cloudy"){
+        thirdForecastIcon.dataset.icon = "fluent:weather-cloudy-48-filled";
+    } else  thirdForecastIcon.dataset.icon ="material-symbols:wb-sunny-outline-rounded";
+
+    // displaying day 4 of 5 forecast
+
+    fourthDayDate.innerHTML = fourthForecastDate;
+    document.getElementById("fourth_forecast_temp").innerHTML = "Temp: " + fourthForecastTemp + '&#8451';
+    document.getElementById("fourth_forecast_wind").innerHTML = "Wind: " + fourthForecastWind+ 'MPH';
+    document.getElementById("fourth_forecast_humidity").innerHTML = "Humidity: " + fourthForecastHumidity;
+
+    var fourthForecastIcon = document.getElementById("fourth_forecast_icon")
+    if (fourthForecastConditions === "rain"){
+         fourthForecastIcon.dataset.icon = "fa-solid:cloud-rain";
+    } else if (fourthForecastConditions === "cloudy"){
+        fourthForecastIcon.dataset.icon = "fluent:weather-cloudy-48-filled";
+    } else  fourthForecastIcon.dataset.icon ="material-symbols:wb-sunny-outline-rounded";
+
+    // displaying day 5 of 5 forecast
+    fifthDayDate.innerHTML = fifthForecastDate;
+    document.getElementById("fifth_forecast_temp").innerHTML = "Temp: " + fifthForecastTemp + '&#8451';
+    document.getElementById("fifth_forecast_wind").innerHTML = "Wind: " + fifthForecastWind+ 'MPH';
+    document.getElementById("fifth_forecast_humidity").innerHTML = "Humidity: " + fifthForecastHumidity;
+
+    var fifthForecastIcon = document.getElementById("fifth_forecast_icon")
+    if (fifthForecastConditions === "rain"){
+         fifthForecastIcon.dataset.icon = "fa-solid:cloud-rain";
+    } else if (fifthForecastConditions === "cloudy"){
+        fifthForecastIcon.dataset.icon = "fluent:weather-cloudy-48-filled";
+    } else  fifthForecastIcon.dataset.icon ="material-symbols:wb-sunny-outline-rounded";
+
+};
 
 var formSubmitHandler = function(event) {
     // prevent page from refreshing
@@ -223,12 +309,91 @@ var getWeatherResults = function(citySearchEntered) {
             } else secondForecastConditions = "sunny";
             console.log("Cloudiness is " + secondDayClouds + ". second Day Rain is " + secondDayRain + ". second Day conditions are " + secondForecastConditions );
 
+            // third day forecast temperatures being pulled and set
+
+            var third_forecast = data.daily[3].dt;
+            console.log("The Third Day in Unix format is" + third_forecast);
+            
+            var thirdDayTemp = data.daily[3].temp.day;
+            thirdForecastTemp = thirdDayTemp;
+            console.log("the third day tempearture is " + thirdDayTemp);
+
+            var thirdDayWind = data.daily[3].wind_speed;
+            thirdForecastWind = thirdDayWind;
+            console.log("The third day wind is " + thirdDayWind);
+
+            var thirdDayHumidity = data.daily[3].humidity;
+            thirdForecastHumidity = thirdDayHumidity;
+            console.log("The third day humidity is " + thirdDayHumidity);
+
+            var thirdDayClouds = parseInt(data.daily[3].clouds);
+            var thirdDayRain = parseFloat((data.daily[3].pop).toFixed(2));
+
+            // checking to see chance of clouds, rain or sun
+            if (thirdDayClouds >50) {
+                thirdForecastConditions ="cloudy";
+            } else if (thirdDayRain > 0.60) {
+                thirdForecastConditions = "rain";
+            } else thirdForecastConditions = "sunny";
+            console.log("Cloudiness is " + thirdDayClouds + ". Third Day Rain is " + thirdDayRain + ". Third Day conditions are " + thirdForecastConditions );
 
 
+               // fourth day forecast temperatures being pulled and set
 
-
+               var fourth_forecast = data.daily[4].dt;
+               console.log("The Fourth Day in Unix format is" + fourth_forecast);
+               
+               var fourthDayTemp = data.daily[4].temp.day;
+               fourthForecastTemp = fourthDayTemp;
+               console.log("the fourth day tempearture is " + fourthDayTemp);
+   
+               var fourthDayWind = data.daily[4].wind_speed;
+               fourthForecastWind = fourthDayWind;
+               console.log("The fourth day wind is " + fourthDayWind);
+   
+               var fourthDayHumidity = data.daily[4].humidity;
+               fourthForecastHumidity = fourthDayHumidity;
+               console.log("The fourth day humidity is " + fourthDayHumidity);
+   
+               var fourthDayClouds = parseInt(data.daily[4].clouds);
+               var fourthDayRain = parseFloat((data.daily[4].pop).toFixed(2));
+   
+               // checking to see chance of clouds, rain or sun
+               if (fourthDayClouds >50) {
+                   fourthForecastConditions ="cloudy";
+               } else if (fourthDayRain > 0.60) {
+                   fourthForecastConditions = "rain";
+               } else fourthForecastConditions = "sunny";
+               console.log("Cloudiness is " + fourthDayClouds + ". Fourth Day Rain is " + fourthDayRain + ". Fourth Day conditions are " + fourthForecastConditions );
             
             
+                // fifth day forecast temperatures being pulled and set
+
+                var fifth_forecast = data.daily[5].dt;
+                console.log("The Fifth Day in Unix format is" + fifth_forecast);
+                
+                var fifthDayTemp = data.daily[5].temp.day;
+                fifthForecastTemp = fifthDayTemp;
+                console.log("the fifth day tempearture is " + fifthDayTemp);
+    
+                var fifthDayWind = data.daily[5].wind_speed;
+                fifthForecastWind = fifthDayWind;
+                console.log("The fifth day wind is " + fifthDayWind);
+    
+                var fifthDayHumidity = data.daily[5].humidity;
+                fifthForecastHumidity = fifthDayHumidity;
+                console.log("The fifth day humidity is " + fifthDayHumidity);
+    
+                var fifthDayClouds = parseInt(data.daily[5].clouds);
+                var fifthDayRain = parseFloat((data.daily[5].pop).toFixed(2));
+    
+                // checking to see chance of clouds, rain or sun
+                if (fifthDayClouds >50) {
+                    fifthForecastConditions ="cloudy";
+                } else if (fifthDayRain > 0.60) {
+                    fifthForecastConditions = "rain";
+                } else fifthForecastConditions = "sunny";
+                console.log("Cloudiness is " + fifthDayClouds + ". fifth Day Rain is " + fifthDayRain + ". Fifth Day conditions are " + fifthForecastConditions );
             console.log("The curent temperature is " + data.current.temp);
             displayCurrentWeather();
         });
